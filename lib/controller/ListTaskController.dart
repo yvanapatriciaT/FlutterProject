e0a0dccb1b428c8dbf204d946cd6668550dc0cb4
yvanaPT <101006561+yvanaPT@users.noreply.github.com>
2023-05-30 09:52:11 +0200

    Controllers

---
+++ lib/controller/ListTaskController.dart
@@ -0,0 +1,12 @@
+import 'package:projet_flutter_i2_g5/data/FireStoreDB.dart';
+import '../model/Task.dart';
+
+class ListTaskController {
+
+  ListTaskController();
+
+  Future<List<Task>> listTask (){
+    FireStoreBD fireStoreBD = FireStoreBD();
+    return fireStoreBD.fetchTasks();
+  }
+}
