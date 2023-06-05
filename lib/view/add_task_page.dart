90602874ad57518716fffb8d7ef5482f5a1e96ad
yvanaPT <101006561+yvanaPT@users.noreply.github.com>
2023-06-05 20:55:57 +0200

    ajout d'une tache, front et back

--- lib/view/add_task_page.dart
+++ lib/view/add_task_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:projet_flutter_i2_g5/controller/AddTaskController.dart';
-import 'package:projet_flutter_i2_g5/view/ToDoList.dart';
+import 'package:projet_flutter_i2_g5/controller/add_task_controller.dart';
+import 'package:projet_flutter_i2_g5/view/to_do_list_page.dart';
 
-import 'TextAndField.dart';
+import 'text_and_field.dart';
 
 class AddTaskPage extends StatefulWidget {
   const AddTaskPage({Key? key}) : super(key: key);
@@ -30,6 +30,7 @@ class _AddTaskPageState extends State<AddTaskPage> {
   Widget build(BuildContext context) {
     AddTaskController addTaskController = AddTaskController(context, _titleController, _dateController, _descriptionController);
     return Scaffold(
+      backgroundColor: Colors.green[50],
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -57,7 +58,8 @@ class _AddTaskPageState extends State<AddTaskPage> {
                         readOnly: true,
                         onTap: () => _selectDate(context),
                         decoration: const InputDecoration(
-                            border: OutlineInputBorder()
+                            border: OutlineInputBorder(),
+                            hoverColor: Colors.green,
                         ),
                       ),
                   ),
@@ -85,7 +87,7 @@ class _AddTaskPageState extends State<AddTaskPage> {
                                   Navigator.pop(context); // Fermer la boîte de dialogue
                                   Navigator.push(
                                     context,
-                                    MaterialPageRoute(builder: (context) => const ToDoList()) // Aller à la page d'ajout
+                                    MaterialPageRoute(builder: (context) =>  const ToDoList()) // Aller à la page d'ajout
                                   );
                                 },
                                 child: const Text('OK'),
@@ -101,7 +103,13 @@ class _AddTaskPageState extends State<AddTaskPage> {
                 ),
                 const SizedBox(width: 8.0),
                 TextButton(
-                  onPressed:() => addTaskController.cancelTask(),
+                  onPressed:() {
+                    addTaskController.cancelTask();
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) =>  const ToDoList()) // Aller à la page d'ajout
+                    );
+                  },
                   style: ButtonStyle(
                     foregroundColor: MaterialStateProperty.all<Color>(Colors.red), // Pour changer la couleur du texte du bouton
                   ),
